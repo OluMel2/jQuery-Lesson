624072727cd2baf01aa7eea5363fc49b2e8435a2
OluMel2 <129649026+OluMel2@users.noreply.github.com>
2023-08-14 11:30:57 +0000

    6章 【イベントを検出しよう】class属性を追加する/解除する

--- app6.js
+++ app6.js
@@ -19,11 +19,31 @@
 
 
 //マウスアウトされた時に赤色に変化する処理を追加
+//$(function(){
+//  $('.box1').mouseover(function(){
+//    $('.box1').css({'background-color': '#0000FF'});
+//  });
+//  $('.box1').mouseout(function(){
+//    $('.box1').css({'background-color': '#FF0000'});
+//  });
+//});
+
+
+//class属性を追加する/解除する
+
+//上記の記述では、マウスカーソルのイベントを検出して、その都度、CSSを変更させており、すべてのCSSをいちいち書かねばならず、手間がかかる。
+//そこで、マウスオーバー時のCSS（class）をあらかじめ定義しておき、マウスカーソルの状況に応じて、そのCSSを追加（または解除）するように設定した。
+//app.cssファイルとapp.jsファイルを、以下のように書き換える。
+
 $(function(){
   $('.box1').mouseover(function(){
-    $('.box1').css({'background-color': '#0000FF'});
+    $('.box1').addClass('box1-ext');
   });
   $('.box1').mouseout(function(){
-    $('.box1').css({'background-color': '#FF0000'});
+    $('.box1').removeClass('box1-ext');
   });
 });
+
+//class属性の切り替えに使われているのが、addClass( )とremoveClass( )です。
+//addClass( )では、対象のHTML要素にclass属性を追加。
+//removeClass( )では、要素に設定されているclass属性が解除。
